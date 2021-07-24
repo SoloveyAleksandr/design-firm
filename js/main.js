ec0949ff24279f23b562f448017c65efae73a526
SoloveyAleksandr <87898636+SoloveyAleksandr@users.noreply.github.com>
2021-07-24 13:59:34 +0300

    new-project

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function () {
+
+    $('.projects-slider').slick({
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+
+})
+
+const headerBtn = document.getElementById('navListBtn');
+const popup = document.getElementById('popup');
+const popupBg = document.getElementById('popup-bg');
+const form = document.getElementById('popup-form');
+const mainBtn = document.getElementById('main-screen__btn');
+
+headerBtn.addEventListener('click', activePopup);
+mainBtn.addEventListener('click', activePopup);
+
+popupBg.addEventListener('click', deactivePopup);
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+}
+
+function activePopup() {
+    popup.classList.add('_active');
+}
+
+function deactivePopup() {
+    popup.classList.remove('_active');
+}
